d33ac2e034fee398d0ec526a8c379d169365144b
Daniel C <brikbusters@gmail.com>
2026-08-24 22:32:54 -0400

    update toolchain, statically link libc

--- toolchain/toolchain.cmake
+++ toolchain/toolchain.cmake
@@ -5,16 +5,16 @@ set(CMAKE_SYSROOT /usr/share/wasi-sysroot)
 set(WASI_TARGET_FLAGS "--target=wasm32-wasip1")
 set(WASI_SHIM "-include ${CMAKE_CURRENT_LIST_DIR}/include/shim_sockets.h")
 
-set(CMAKE_C_FLAGS                  "${WASI_TARGET_FLAGS} -D_WASI_EMULATED_GETPID -nostdlib ${WASI_SHIM}" CACHE INTERNAL "")
+set(CMAKE_C_FLAGS                  "${WASI_TARGET_FLAGS} -D_WASI_EMULATED_GETPID ${WASI_SHIM}" CACHE INTERNAL "")
 set(CMAKE_C_COMPILER_TARGET        "wasm32-wasip1")
 set(CMAKE_C_COMPILER               "/usr/bin/clang")
 
-set(CMAKE_CXX_FLAGS                "${WASI_TARGET_FLAGS} -D_WASI_EMULATED_GETPID -nostdlib ${WASI_SHIM}" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS                "${WASI_TARGET_FLAGS} -D_WASI_EMULATED_GETPID ${WASI_SHIM}" CACHE INTERNAL "")
 set(CMAKE_CXX_COMPILER_TARGET      "wasm32-wasip1")
 set(CMAKE_CXX_COMPILER             "/usr/bin/clang++")
 
-set(CMAKE_EXE_LINKER_FLAGS "-Wl,--initial-memory=65536,--no-entry,--export=get_module" CACHE INTERNAL "")
-set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--initial-memory=65536,--no-entry" CACHE INTERNAL "")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--export-table,--no-entry,--export=get_module" CACHE INTERNAL "")
+set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--export-table,--no-entry" CACHE INTERNAL "")
 
 set(CMAKE_LINKER  "/usr/bin/wasm-ld"                     CACHE INTERNAL "")
 set(CMAKE_AR      "/usr/bin/llvm-ar"                     CACHE INTERNAL "")
